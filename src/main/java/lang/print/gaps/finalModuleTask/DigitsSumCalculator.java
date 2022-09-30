cce289cf46f24914d5c25af2be340cfda9a480b5
IsmoiljonJabborov <190107106@stu.sdu.edu.kz>
2022-10-01 00:01:44 +0500

    Honestly, tasks are unclear enough!

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,15 +3,5 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public static int calculateSum(int number) {
         return number == 0 ? 0 : number % 10 + calculateSum(number/10);
-
-//        int sum = 0;
-//        int lastDigit = number % 10;
-//        sum += lastDigit;
-//        number = number / 10;
-//        System.out.println(sum);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(calculateSum(1234));
     }
 }
