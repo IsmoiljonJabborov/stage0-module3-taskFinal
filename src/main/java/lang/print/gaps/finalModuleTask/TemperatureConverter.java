e0d19e408f90faf3aa42002eead551953b258e72
IsmoiljonJabborov <190107106@stu.sdu.edu.kz>
2022-09-30 12:35:25 +0500

    There are two of them is not completed yet!

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public static void toFahrenheit(int temperatureCelsius){
+        System.out.println(temperatureCelsius * 1.8 + 32);
+    }
+
+    public static void main(String[] args) {
+        toFahrenheit(32);
     }
 }
